e5fc8e2889c10450d7432002ccef43854b3b1be2
Sergey <kanishevskiy@gmail.com>
2021-12-12 18:17:28 +0300

    homework 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  }
+  else if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+  else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    return arr;
+  }
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let credit = amount - contribution;
+  let current_date = new Date();
+  let months = (date.getFullYear() - current_date.getFullYear()) * 12 - current_date.getMonth() + date.getMonth();
+  let interest_rate = (1 / 12) * (percent / 100);
+  let payment = credit * (interest_rate + (interest_rate / (Math.pow(1 + interest_rate, months) - 1)));
+  totalAmount = payment * months;
+  return +totalAmount.toFixed(2);
 }
