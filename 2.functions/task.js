987457e56c281dce2af318675a3258a2f8f16259
Sergey <kanishevskiy@gmail.com>
2022-02-03 12:47:10 +0300

    решение

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+	let min = 5;
+	let max = 70;
+	let sum = 0;
+	let avg = 0;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
-}
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      arr[i] = max;
+    }
+    if (arr[i] < min) {
+      arr[i] = min;
+    };
+    sum += arr[i];
+  }
+  avg = sum/arr.length;
+  avg = Number(avg.toFixed(2));
+  return { min, max, avg };
+};
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
+  let sum;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++){
+    sum = func(arrOfArr[i]);
+    if (max < sum) {
+      max = sum
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = 0;
+  let max = 0;
+  let difference;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  difference = max - min;
+  return Math.abs(difference);
 }
