b6796b2c95e93e97322c54a05b1cae994eac564f
Sergey <kanishevskiy@gmail.com>
2022-05-05 18:56:48 +0300

    homeworks

---
+++ 5.classes/task.js
@@ -0,0 +1,95 @@
+class PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+    }
+
+    set state(state){
+        if (state < 0) {
+            this._state = 0;
+        } else if (state > 100) {
+            this._state = 100;
+        } else {
+            this._state = state;
+        }
+    }
+    
+    get state(){
+        return this._state;
+    }
+
+}
+
+class Magazine extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount,author) {
+        super(name, releaseDate, pagesCount,author);
+        this.type = 'book';
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book {
+    constructor (name, releaseDate, pagesCount,author) {
+        super(name, releaseDate, pagesCount,author);
+        this.type = 'novel';
+    }
+}
+
+class FantasticBook extends Book {
+    constructor (name, releaseDate, pagesCount,author) {
+        super(name, releaseDate, pagesCount,author);
+        this.type = 'fantastic';
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor (name, releaseDate, pagesCount,author) {
+        super(name, releaseDate, pagesCount,author);
+        this.type = 'detective';
+    }
+}
+
+class Library {
+    constructor (name, books) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        for (let book of this.books) {
+            if (book.[type] === value) {
+                return book; 
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        for (let index of this.books) { 
+            if (this.books[index].name === bookName) {
+                return this.books.splice(index, 1);
+            }
+        }   
+        return null; 
+    }
+}
